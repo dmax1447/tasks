b66accf020655dc8c9a97e479a9bcbbece255ae7
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2022-11-01 17:52:21 +0300

    add twoSums

---
+++ src/tasks/twoSum.js
@@ -0,0 +1,18 @@
+// функция должна вернуть индексы двух элементов из массива, которые в сумме дадут заданное число
+// решить за 1 проход по массиву
+const numbers = [1,6,9,13,26]
+
+function twoSum(nums, target) {
+    const hashTable = {}
+    for (let idx = 0; idx < nums.length; idx++) {
+        const el = nums[idx]
+        hashTable[target - el] = idx
+        if (hashTable[el]) {
+            return [hashTable[el], idx]
+        }
+    }
+    return new Error('no pair')
+}
+
+const indexes = twoSum(numbers, 15)
+
