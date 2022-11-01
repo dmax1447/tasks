456c6966f9ad7c2e05ce73a35b6c4d5341050621
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2022-11-01 16:54:52 +0300

    add reduceString

---
+++ src/tasks/svertka.js
@@ -0,0 +1,28 @@
+const str = 'aascsdfggbnnddddfff'
+
+function reduceString(str) {
+    if (str.length || str.length === 1) {
+        return str
+    }
+
+    let stack = ''
+    let result = ''
+
+    stack = str[0]
+    stack.length
+
+    for (let i = 1; i < str.length; i++) {
+        const char = str[i]
+        if (stack[0] !== char) {
+            result += `${stack[0]}${stack.length}`
+            stack = char
+        } else {
+            stack += char
+        }
+    }
+    return result
+}
+
+const result = reduceString(str)
+
+
