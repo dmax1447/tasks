f9191ab0ea26b4375f2fad10c797159e622fb962
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2023-12-27 15:23:18 +0300

    wip context 03

--- src/contest_3B/03_collector_Diego.js
+++ src/contest_3B/03_collector_Diego.js
@@ -1,21 +1,39 @@
-function calc({N, stickers, K, stickerMaximums}) {
-  console.log({N, stickers, K, stickerMaximums})
+// function calc({N, stickers, K, stickerMaximums}) {
+//   console.log({N, stickers, K, stickerMaximums})
+//
+//   const diegoCards = [...new Set(stickers.map(Number).sort((a, b) => a - b))]
+//   console.log(diegoCards)
+// }
+//
+// //
+// process.stdin.resume();
+// process.stdin.setEncoding('utf8');
+// let rawStr = ''
+// process.stdin.on('data', data => {
+//   rawStr += data.toString()
+// });
+// process.on('exit', (code) => {
+//   const [N, stickersRaw, K,  stickerMaximumsRaw] = rawStr.split('\n')
+//   calc({N, stickers: stickersRaw.split(' '), K, stickerMaximums: stickerMaximumsRaw.split(' ')})
+// });
 
-  const diegoCards = [...new Set(stickers.sort((a, b) => a - b))]
-  console.log(diegoCards)
+const arr = [1, 3, 5, 8, 23, 42, 50, 56, 71, 98, 100, 198, 300]
+const arr2 = [1, 4]
+const arr3 = [1, 50, 100]
+let iter = 0
+function findIdxLessThenTarget(arr, target, start, end) {
+  const subsequenceLength = (end - start) + 1
+  if (subsequenceLength === 2) {
+
+    return target > arr[start] ? start : end
+  }
+  const middle = start + Math.floor((end - start) / 2)
+  if (target > arr[middle]) {
+    return findIdxLessThenTarget(arr, target, middle, end)
+  } else {
+    return findIdxLessThenTarget(arr, target, start, middle)
+  }
 }
 
-//
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-let rawStr = ''
-process.stdin.on('data', data => {
-  rawStr += data.toString()
-});
-process.on('exit', (code) => {
-  const [N, stickersRaw, K,  stickerMaximumsRaw] = rawStr.split('\n')
-  calc({N, stickers: stickersRaw.split(' '), K, stickerMaximums: stickerMaximumsRaw.split(' ')})
-});
-
-
-
+const idx = findIdxLessThenTarget(arr, 75, 0, arr.length - 1) //?
+arr.slice(0, idx) //?
