8a922ad8f0875996645b29b18adf58f10099a329
Max Dragaytsev <dragaytsev@air.agency>
2024-01-14 19:33:37 +0300

    contest 3 wip

--- src/contest_3B/03_collector_Diego.js
+++ src/contest_3B/03_collector_Diego.js
@@ -1,39 +1,47 @@
-// function calc({N, stickers, K, stickerMaximums}) {
-//   console.log({N, stickers, K, stickerMaximums})
-//
-//   const diegoCards = [...new Set(stickers.map(Number).sort((a, b) => a - b))]
-//   console.log(diegoCards)
-// }
-//
-// //
-// process.stdin.resume();
-// process.stdin.setEncoding('utf8');
-// let rawStr = ''
-// process.stdin.on('data', data => {
-//   rawStr += data.toString()
-// });
-// process.on('exit', (code) => {
-//   const [N, stickersRaw, K,  stickerMaximumsRaw] = rawStr.split('\n')
-//   calc({N, stickers: stickersRaw.split(' '), K, stickerMaximums: stickerMaximumsRaw.split(' ')})
-// });
-
-const arr = [1, 3, 5, 8, 23, 42, 50, 56, 71, 98, 100, 198, 300]
-const arr2 = [1, 4]
-const arr3 = [1, 50, 100]
-let iter = 0
 function findIdxLessThenTarget(arr, target, start, end) {
   const subsequenceLength = (end - start) + 1
-  if (subsequenceLength === 2) {
-
-    return target > arr[start] ? start : end
-  }
   const middle = start + Math.floor((end - start) / 2)
-  if (target > arr[middle]) {
+  if (subsequenceLength <= 3) {
+    if (arr[end] <= target) return end
+    if (arr[middle] <= target) return middle
+    if (arr[start] <= target) return start
+    return null
+  }
+
+  if (target >= arr[middle]) {
     return findIdxLessThenTarget(arr, target, middle, end)
+
   } else {
     return findIdxLessThenTarget(arr, target, start, middle)
   }
 }
 
-const idx = findIdxLessThenTarget(arr, 75, 0, arr.length - 1) //?
-arr.slice(0, idx) //?
+function count(num, arr) {
+  const idx = findIdxLessThenTarget(arr, num, 0, arr.length - 1)
+  return idx === null ? 0 : idx + 1
+}
+function calc({N, stickers, K, stickerMaximums}) {
+
+  const diegoCards = [...new Set(stickers.map(Number).sort((a, b) => a - b))]
+  console.log({N, stickers, K, stickerMaximums, diegoCards})
+  stickerMaximums.forEach((max) => {
+    console.log(count(max, diegoCards))
+  })
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+let rawStr = ''
+process.stdin.on('data', data => {
+  rawStr += data.toString()
+});
+process.on('exit', (code) => {
+  const [N, stickersRaw, K,  stickerMaximumsRaw] = rawStr.split('\n')
+  calc({N, stickers: stickersRaw.split(' '), K, stickerMaximums: stickerMaximumsRaw.split(' ')})
+});
+
+// const arr = [2, 50, 100, 105]
+// const res = count(100, arr) //?
+
+
